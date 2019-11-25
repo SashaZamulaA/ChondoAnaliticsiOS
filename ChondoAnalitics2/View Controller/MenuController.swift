d0273e9dd8aaffc2e83502e9bce62656c018f271
SashaZamulaA <z.sasha1@gmail.com>
2019-11-25 11:17:09 +0200

    25.11.2019

---
+++ ChondoAnalitics2/View Controller/MenuController.swift
@@ -0,0 +1,22 @@
+//
+//  MenuController.swift
+//  ChondoAnalitics2
+//
+//  Created by ZamulaAleksandr on 15.11.2019.
+//  Copyright © 2019 Alex Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+//MARK: - Properties
+//MARK: - Init
+override func viewDidLoad() {
+       super.viewDidLoad()
+    
+}
+
+//MARK: - Handlers
+
+}
